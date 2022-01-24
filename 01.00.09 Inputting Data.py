5bad8dcb053734611a0dc904c5b279d99541f7d3
Garrett-Morgan <gamorgan@UALR.EDU>
2022-01-24 16:16:34 +0000

    class today

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,7 @@
+firstname = input("Pease Enter Your First Name: ")  # Input statement without a prompt
+print("You Entered: " + firstname)
+
+lastname = input("Enter Your Last Name: ")  # Input statement with a prompt
+print("You Entered: " + lastname)
+
+print("You Entered: " + firstname + lastname)

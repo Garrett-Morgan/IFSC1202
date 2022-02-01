505b71214aa324513974a4c75da2862d0bc10091
Garrett-Morgan <gamorgan@UALR.EDU>
2022-02-01 17:07:17 +0000

    wet

--- 01.00.06 Python Strings.py
+++ 01.00.06 Python Strings.py
@@ -1,23 +1,9 @@
-# String literals are surrounded by either single quotation marks, 
-# or double quotation marks. 
-print("Hello")
-print('Hello')
-
-# Blank Line
-print("")
-
-# Example of a multiline string
-a = """Lorem ipsum dolor sit amet, 
-consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt 
-ut labore et dolore magna aliqua."""
-print(a)
-
-# Blank Line
-print("")
-
-# Concatenating strings
-a = "Hello"
-b = "Dolly"
-c = a + " " + b + "!"
-print(c)
+x = int(input("Enter First Number: "))
+y = int(input("Enter Second Number: "))
+z = int(input("Enter Third Number: "))
+if(x < y):
+    if (x < z): print("{}".format(x))    
+if(y < x):
+    if (y < z): print("{}".format(y))
+if(z < x):
+    if (z < y): print("{}".format(z))

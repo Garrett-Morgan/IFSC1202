1dd8ac9e3b9b275292761ca1f509c9d6dbe35f28
Garrett-Morgan <gamorgan@UALR.EDU>
2022-02-02 16:02:22 +0000

    sgfdfdgs

---
+++ 03.08 Days In Month.py
@@ -0,0 +1,18 @@
+x = int(input("Enter Digit Month: "))
+if(x == 1): print("31")
+if(x == 3): print("31")
+if(x == 5): print("31")
+if(x == 7): print("31")
+if(x == 8): print("31")
+if(x == 10): print("31")
+if(x == 12): print("31")
+
+if(x == 4): print("30")
+if(x == 6): print("30")
+if(x == 9): print("30")
+if(x == 11): print("30")
+
+if(x == 2): print ("28")
+
+if(x < 0): print("Not Month")
+if(x >12): print("Not Month")

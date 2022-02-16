37e81d30bb5a090e7ae3acbf42401336de6ed9a2
Garrett-Morgan <gamorgan@UALR.EDU>
2022-02-16 01:21:47 +0000

    osdfon

---
+++ 04.07 Ladder.py
@@ -0,0 +1,6 @@
+x = int(input("Enter Number: "))
+if(x > 9): print("Number too large") (quit())
+if(x < 0): print("Number too small") (quit())
+if( x <= 9):
+    for i in range(1, x+1):
+        print(i)
---
+++ 04.08 Is Prime.py
@@ -0,0 +1,5 @@
+x=int(input("Enter Number: "))
+for i in range(2, x // 2 + 1):
+    result = x / i
+    if (result % 1 == 0 ): print("COMPOSITE") (quit())
+else: print("PRIME")
